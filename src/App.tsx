232afd0c972e2d625d4d822a52dd6986ffeb0620
devrafaelsoares <rafael.soares.developer@gmail.com>
2023-10-14 14:36:19 -0300

    add: implementing countdown context

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import "./App.css";
 import Countdown from "./components/Countdown";
 import Title from "./components/Countdown/components/Title";
 import Modal from "./components/Modal";
+import AppProvider from "./context";
 
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col justify-center gap-3 bg-primaryDark">
-      <Title />
-      <Countdown />
-      <Modal />
+      <AppProvider>
+        <Title />
+        <Countdown />
+        <Modal />
+      </AppProvider>
     </div>
   );
 }
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,7 +1,24 @@
+import { ReactNode } from "react";
+
 export interface FormProps {
     closeModal: () => void;
 }
 export interface CounterProps {
     name: string;
     time: string;
+}
+
+export interface ChildrenProps {
+    children: ReactNode;
+}
+
+export interface EventProps {
+    title: string;
+    date: string;
+}
+export interface CountdownProps {
+    event: EventProps | null;
+    addEvent: (sendEvent: EventProps) => void;
+    cleanEvent: () => void;
+    getEvent: () => EventProps | null;
 }
