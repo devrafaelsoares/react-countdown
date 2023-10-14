e8ca1f483ad2d211e268328cf06845a946851495
devrafaelsoares <rafael.soares.developer@gmail.com>
2023-10-14 14:32:44 -0300

    udpdate: linking components and applying additional tailwindcss settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primaryDark: "#121212",
+        secondDark: "#1E1E1E",
+      },
+      colors: {
+        primaryBlue: "#0099EE",
+      },
+    },
   },
   plugins: [],
 }
